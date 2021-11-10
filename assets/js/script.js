cd267499ea8d1a11d954728130fe92b75e1da0e8
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-09 23:15:11 -0600

    installed webpack to automate minifying and application build process, created main.bundle.js from the build process

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+require("bootstrap");
+
+$(function() {
   function createEl(htmlString, attrs, ...children) {
     if (typeof htmlString !== "string") {
       throw Error("Argument 'htmlString' is required and must be a string");
