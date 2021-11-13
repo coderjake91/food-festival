d18a24cbdc1c1cf7b9f23dde4b8438e4299699e7
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-12 21:23:29 -0600

    added event listeners for activate and fetch events in the navigator to handle off-line cache service of site

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,10 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./dist/index.html",
-    "./dist/events.html",
-    "./dist/tickets.html",
-    "./dist/schedule.html",
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
     "./assets/css/style.css",
     "./assets/css/bootstrap.css",
     "./assets/css/tickets.css",
@@ -17,6 +17,7 @@ const FILES_TO_CACHE = [
   ];
 
 
+//install the cache and store the files to the cache
 self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
@@ -24,4 +25,34 @@ self.addEventListener('install', function (e) {
         return cache.addAll(FILES_TO_CACHE)
       })
     )
-  })
+  });
+
+//manage existing cache information, and save new cache information with the app prefix
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keylist) {
+            let cacheKeeplist = keylist.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keylist.map(function (key, i) {
+                    if(cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache: ' + keylist[i]);
+                        return caches.delete(keylist[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request: ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request);
+        })
+    );
+});
